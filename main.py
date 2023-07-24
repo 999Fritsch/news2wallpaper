67c3afb3fc2044cb4d14a250d43c446ddfdaac8a
Fritsch <filip.fritsch@gmail.com>
2023-07-24 21:09:03 +0200

    #2 added filtered german news refactored some of the code

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import json
-import requests
-import io
-import base64
 from PIL import Image, PngImagePlugin
 from datetime import date
 from pathlib import Path
@@ -11,7 +8,7 @@ import os
 from dotenv import load_dotenv
 import webuiapi
 
-def genImage(prompt, date):
+def genImage(prompt, output_path):
 
     negative_prompt = "CyberRealistic_Negative, ((worst quality, low quality), bad_pictures, negative_hand-neg:1.2)"
 
@@ -33,15 +30,15 @@ def genImage(prompt, date):
 
     image_name =  ''.join(filter(str.isalnum, prompt))
 
-    path = Path(f"images/{date}/image_{image_name}.png")
+    path = Path(f"{output_path}/image_{image_name}.png")
     path.touch()
 
     image.save(path, pnginfo=pnginfo)
 
 
-def get_headlines(date):
+def get_articles(path, amount):
 
-    if not os.path.exists(f"images/{date}/articles.json"):
+    if not os.path.exists(f"{path}/articles.json"):
 
         print("Getting todays news from api...")
 
@@ -49,27 +46,46 @@ def get_headlines(date):
         newsapi = NewsApiClient(api_key=os.getenv('NEWS_API_KEY'))
 
         # /v2/top-headlines
-        top_headlines = newsapi.get_top_headlines(
-                                            page_size=10,
+        top_articles = newsapi.get_top_headlines(
+                                            page_size=amount,
                                             language="de"
                                               )
 
-        with open(f"images/{date}/articles.json", "w") as file:
-            json.dump(top_headlines, file, indent=4)
+        with open(f"{path}/articles.json", "w") as file:
+            json.dump(top_articles, file, indent=4)
 
     else:
 
-        print(f"Getting todays news from images/{date}/articles.json...")
+        print(f"Getting todays news from {path}/articles.json...")
 
-        with open(f"images/{date}/articles.json") as file:
-            top_headlines = json.load(file)
-        
-    headlines_list = []
-    for article in top_headlines["articles"]:
+        with open(f"{path}/articles.json") as file:
+            top_articles = json.load(file)
 
-        headlines_list.append(article["title"])
+    return top_articles["articles"]
 
-    return headlines_list
+def filter_articles(articles_file, output_file=None, authors_file="authors.json"):
+
+    if output_file == None:
+        output_file = articles_file
+
+    with open(articles_file) as file:
+            top_articles = json.load(file)
+
+    with open(authors_file) as file:
+            authors_filter = json.load(file)
+
+    # Filter articles based on author and convert to a dictionary
+    filtered_articles = []
+
+    for article in top_articles["articles"]:
+        if article["author"] in authors_filter["authors"]:
+            filtered_articles.append(article)
+
+    with open(f"{output_file}", "w") as file:
+            json.dump({"articles":filtered_articles}, file, indent=4)
+
+    return filtered_articles
+
 
 def create_today_images():
 
@@ -77,17 +93,20 @@ def create_today_images():
 
     print(f"today is {date_today}")
 
-    Path(f"images/{date_today}").mkdir(parents=True, exist_ok=True)
+    path = Path(f"images/{date_today}")
+    path.mkdir(parents=True, exist_ok=True)
 
-    headlines = get_headlines(date_today)
+    get_articles(path, 100)
 
-    print(f"got {len(headlines)} headlines")
+    filtered_articles = filter_articles(path.joinpath("articles.json"))
 
-    for headline in tqdm(headlines):
+    print(f"got {len(filtered_articles)} headlines")
 
-        genImage(headline, date_today)
+    for article in tqdm(filtered_articles):
 
-        print(f"\ncreated image for:\n{headline}\n")
+        genImage(article["title"], path)
+
+        print(f"\ncreated image for:\n{article['title']}\n")
 
 if __name__ == "__main__":
 
@@ -95,6 +114,10 @@ if __name__ == "__main__":
 
     load_dotenv()
 
-    create_today_images()
+    # create_today_images()
 
     # genImage("cute cat", date.today().strftime("%Y/%m/%d"))
+
+    # filter_articles("test/articles.json", "authors copy.json", "test/filtered_articles.json")
+
+    create_today_images()
